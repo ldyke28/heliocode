daf14cd20e482a366404c6e9e90f1fd27f833633
ldyke28 <87095065+ldyke28@users.noreply.github.com>
2022-07-05 14:35:15 -0400

    Testing
    More testing

--- pstraj.py
+++ pstraj.py
@@ -9,7 +9,7 @@ from mpl_toolkits import mplot3d
 # 1 = generate a list of trajectories that come within proximity
 # 2 = plot an individual trajectory traced backward from point of interest
 # 3 = generate phase space diagram
-mode = 3
+mode = 2
 #contourplot = True # determines whether scatter (False) or contour (True) plot
 
 # Value for 1 au (astronomical unit) in meters
@@ -20,7 +20,7 @@ G = 6.6743*10**(-11) # value for gravitational constant in SI units
 # Location of the sun in [x,y,z] - usually this will be at 0, but this makes it flexible just in case
 # Second line is location of the point of interest in the same format (which is, generally, where we want IBEX to be)
 sunpos = np.array([0,0,0])
-ibexpos = np.array([-.707*au, .707*au, 0])
+ibexpos = np.array([-.866*au, .5*au, 0])
 
 # INITIAL CONDITIONS for both position and velocity (in SI units - m and m/s)
 ttotal = 7000000000
@@ -55,8 +55,8 @@ ystart = ibexpos[1]
 zstart = ibexpos[2]
 #vxstart = np.arange(-50000, 10000, 1200)
 #vystart = np.arange(-35000, 0000, 1000)
-vxstart = np.arange(-25000, 20000, 700)
-vystart = np.arange(-20000, 25000, 700)
+vxstart = np.arange(-25000, 20000, 1000)
+vystart = np.arange(-20000, 25000, 1000)
 #vxstart = np.arange(-50000, 20000, 1500)
 #vystart = np.arange(-50000, 50000, 1500)
 vzstart = 0
@@ -165,7 +165,7 @@ if mode==3:
 
 # single trajectory plotting code
 if mode==2:
-    init = [ibexpos[0], ibexpos[1], ibexpos[2], -12000, 000, 0]
+    init = [ibexpos[0], ibexpos[1], ibexpos[2], 10000, -6000, 0]
     singletraj = odeint(dr_dt, init, t, args=(rp4,))
     for k in range(t.size):
             if singletraj[k,0] >= 100*au:
@@ -216,8 +216,8 @@ if mode==1:
 
 if mode==3:
     # writing data to a file - need to change each time or it will overwrite previous file
-    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/p5s2_meddownwind_sin2=p25_str_center.txt", 'w')
-    #file = open("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/p5s2_5pi6_attractive_ring_highres.txt", "w")
+    #file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/p5s2_meddownwind_sin2=p25_str_center.txt", 'w')
+    file = open("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/p5s2_5pi6_sin2=p25_str_center.txt", "w")
     for i in range(farvx.size):
         file.write(str(farvx[i]/1000) + ',' + str(farvy[i]/1000) + ',' + str(maxwcolor[i]) + '\n')
     file.close()
@@ -236,7 +236,7 @@ if mode==3:
     #plt.suptitle('Phase Space population at x = 100 au reaching initial position at t = 5700000000 s')
     plt.suptitle('Phase space population at target (t = 6.275e9 s) drawn from Maxwellian at 100 au centered on vx = -26 km/s')
     #plt.title('Target (-.97au, .2au): vx range -51500 m/s to -30500 m/s, vy range -30000 m/s to 30000 m/s')
-    plt.title('Target at (-.707 au, .707 au)')
+    plt.title('Target at (-.866 au, .5 au)')
     #plt.title('Initial test distribution centered on vx = -41.5 km/s, vy = -1.4 km/s')
     plt.show()
     
@@ -254,6 +254,6 @@ if mode==3:
     #plt.suptitle('Phase Space population at x = 100 au reaching initial position at t = 5700000000 s')
     plt.suptitle('Phase space population at target (t = 6.275e9 s) drawn from Maxwellian at 100 au centered on vx = -26 km/s')
     #plt.title('Target (-.97au, .2au): vx range -51500 m/s to -30500 m/s, vy range -30000 m/s to 30000 m/s')
-    plt.title('Target at (-.707 au, .707 au)')
+    plt.title('Target at (-.866 au, .5 au)')
     #plt.title('Initial test distribution centered on vx = -41.5 km/s, vy = -1.4 km/s')
     plt.show()
