e060eb804f08b41d47b957ab90ea1ed3fd677396
ldyke28 <87095065+ldyke28@users.noreply.github.com>
2022-08-30 17:25:35 -0400

    Generating plots for paper

--- pstraj.py
+++ pstraj.py
@@ -23,7 +23,7 @@ oneyear = 3.156*10**7
 finalt = 6250000000 # time to start backtracing
 #6.36674976e9 force free for cosexprp
 tstep = 10000 # general time resolution
-tstepclose = 400 # time resolution for close regime
+tstepclose = 200 # time resolution for close regime
 tstepfar = 200000 # time resolution for far regime
 phase = 0 # phase for implementing rotation of target point around sun
 
@@ -68,10 +68,10 @@ zstart = ibexpos[2]
 
 # Multiple sets of initial vx/vy conditions for convenience
 # In order of how I use them - direct, indirect, center, extra one for zoomed testing
-vxstart = np.arange(-55000, -10000, 400)
-vystart = np.arange(-45000, -20000, 200)
-#vxstart = np.arange(-45000, -15000, 300)
-#vystart = np.arange(16500, 30000, 150)
+#vxstart = np.arange(-55000, -10000, 400)
+#vystart = np.arange(-45000, -20000, 200)
+vxstart = np.arange(-30000, 10000, 200)
+vystart = np.arange(40000, 55000, 80)
 #vxstart = np.arange(-25000, 25000, 500)
 #vystart = np.arange(-25000, 25000, 500)
 #vxstart = np.arange(-50000, 0000, 2500)
@@ -281,7 +281,7 @@ if mode==1:
 if mode==3:
     # writing data to a file - need to change each time or it will overwrite previous file
     #file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/cosexprp_5pi6_6p36675e9_direct_cosexppi_test.txt", 'w')
-    file = open("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/norp_5pi6_6p25e9_direct_cosexppi.txt", "w")
+    file = open("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/norp_5pi6_6p25e9_indirect_cosexppi.txt", "w")
     for i in range(farvx.size):
         file.write(str(farvx[i]/1000) + ',' + str(farvy[i]/1000) + ',' + str(maxwcolor[i]) + '\n')
     file.close()
@@ -300,7 +300,7 @@ if mode==3:
     #plt.suptitle('Phase Space population at x = 100 au reaching initial position at t = 5700000000 s')
     plt.suptitle('Phase space population at target (t = 6.25e9 s) drawn from Maxwellian at 100 au centered on vx = -26 km/s')
     #plt.title('Target (-.97au, .2au): vx range -51500 m/s to -30500 m/s, vy range -30000 m/s to 30000 m/s')
-    plt.title('Target at (-.866 au, .5 au), Time Resolution Close to Target = 400 s')
+    plt.title('Target at (-.866 au, .5 au), Time Resolution Close to Target = 200 s')
     #plt.title('Initial test distribution centered on vx = -41.5 km/s, vy = -1.4 km/s')
     plt.show()
     
