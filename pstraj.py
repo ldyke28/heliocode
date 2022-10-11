148435c1d7abf89e37bf249b8781ec7dca7e4fe4
ldyke28 <87095065+ldyke28@users.noreply.github.com>
2022-10-11 18:00:08 -0400

    Additional check
    Added code that forgoes looping through time indices for orbits that never have an x value exceeding 100 au

--- pstraj.py
+++ pstraj.py
@@ -22,7 +22,7 @@ oneyear = 3.156*10**7
 
 # 120749800 for first force free
 # 226250200 for second force free
-finalt = 93000000 # time to start backtracing
+finalt = 23000000 # time to start backtracing
 #6.36674976e9 force free for cosexprp
 initialt = -2000000000
 tstep = 10000 # general time resolution
@@ -77,8 +77,8 @@ zstart = ibexpos[2]
 #vystart = np.arange(-2000, 6500, 150)
 #vxstart = np.arange(-25000, 25000, 500)
 #vystart = np.arange(-25000, 25000, 500)
-vxstart = np.arange(-25000, 25000, 150)
-vystart = np.arange(-25000, 25000, 150)
+vxstart = np.arange(-25000, 25000, 250)
+vystart = np.arange(-25000, 25000, 250)
 vzstart = 0
 if mode==3:
     startt = finalt
@@ -182,6 +182,9 @@ if mode==3:
             if any(np.sqrt((backtraj[:,0]-sunpos[0])**2 + (backtraj[:,1]-sunpos[1])**2 + (backtraj[:,2]-sunpos[2])**2) <= .00465*au):
                 # tells the code to not consider the trajectory if it at any point intersects the width of the sun
                 continue
+            if all(backtraj[:,0]-sunpos[0]) < 100*au:
+                # forgoes the loop for trajectories that never make it to x = 100 aus
+                continue 
             for k in range(t.size - tclose.size):
                 if backtraj[k+tclose.size,0] >= 100*au and backtraj[k-1+tclose.size,0] <= 100*au:
                     # adjusting the indexing to avoid checking in the close regime
@@ -320,17 +323,17 @@ if mode==1:
 
 if mode==3:
     # writing data to a file - need to change each time or it will overwrite previous file
-    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/cosexprp_pi32_9p3e7_center_cosexppi_highspatialres_ex.txt", 'w')
-    #file = open("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/cosexprp_pi32_4p9e8_center_cosexppi_zoom_ex.txt", "w")
+    #file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/cosexprp_pi32_9p3e7_center_cosexppi_highspatialres_ex.txt", 'w')
+    file = open("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/cosexprp_pi32_2p3e7_center_cosexppi_tcolor_higherspatialres.txt", "w")
     for i in range(farvx.size):
-        file.write(str(farvx[i]/1000) + ',' + str(farvy[i]/1000) + ',' + str(maxwcolor[i]) + '\n')
+        file.write(str(farvx[i]/1000) + ',' + str(farvy[i]/1000) + ',' + str(fart[i]) + '\n')
     file.close()
 
     # plotting a scatterplot of vx and vy at the target point, colored by the phase space density
     f = plt.figure()
     f.set_figwidth(10)
     f.set_figheight(6)
-    plt.scatter(farvx[:]/1000, farvy[:]/1000, c=maxwcolor[:], marker='o', cmap='plasma')
+    plt.scatter(farvx[:]/1000, farvy[:]/1000, c=fart[:], marker='o', cmap='plasma')
     cb = plt.colorbar()
     #cb.set_label('Time at which orbit passes through 100 au (s)')
     #cb.set_label('Travel Time from 100 au to Point of Interest (s)')
@@ -340,7 +343,7 @@ if mode==3:
     plt.xlabel("vx at Target in km/s")
     plt.ylabel("vy at Target in km/s")
     #plt.suptitle('Phase Space population at x = 100 au reaching initial position at t = 5700000000 s')
-    plt.suptitle('Phase space population at target (t $\\approx$ 2.942 years) drawn from Maxwellian at 100 au centered on vx = -26 km/s')
+    plt.suptitle('Phase space population at target (t $\\approx$ .724 years) drawn from Maxwellian at 100 au centered on vx = -26 km/s')
     #plt.title('Target (-.97au, .2au): vx range -51500 m/s to -30500 m/s, vy range -30000 m/s to 30000 m/s')
     plt.title('Target at (.9952 au, .0980 au), Time Resolution Close to Target = 1000 s')
     #plt.title('Initial test distribution centered on vx = -41.5 km/s, vy = -1.4 km/s')
