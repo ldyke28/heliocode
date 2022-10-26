e338017dfa5d9b75c4ab197ca7be849e17371f54
ldyke28 <87095065+ldyke28@users.noreply.github.com>
2022-10-25 23:25:48 -0400

    Plotting

--- 3dplot.py
+++ 3dplot.py
@@ -7,8 +7,8 @@ vy = np.array([])
 vz = np.array([])
 f = np.array([])
 
-file = np.loadtxt("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/pi32_2p5e8.txt", delimiter=',')
-#file = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/pi32_2p9e8.txt", delimiter=',')
+#file = np.loadtxt("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/pi32_3p1e8.txt", delimiter=',')
+file = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/pi32_3p1e8.txt", delimiter=',')
 
 #file = file[np.any(file > 1, axis=1)]
 
@@ -18,8 +18,8 @@ vz = file[:,2]
 f = file[:,3]
 
 fig3d = plt.figure()
-fig3d.set_figwidth(15)
-fig3d.set_figheight(11)
+fig3d.set_figwidth(10)
+fig3d.set_figheight(7)
 ax3d = plt.axes(projection='3d')
 #scatterplot = ax3d.scatter3D(vx[:], vy[:], vz[:], c=f[:], cmap='plasma', s=.02, vmin=(.75-.243/np.e), vmax=(.75+.243*np.e))
 scatterplot = ax3d.scatter3D(vx[:], vy[:], vz[:], c=f[:], cmap='plasma', s=.005)
@@ -28,11 +28,11 @@ ax3d.set_xlabel("$v_x$ at Target Point (km/s)")
 ax3d.set_ylabel("$v_y$ at Target Point (km/s)")
 ax3d.set_zlabel("$v_z$ at Target Point (km/s)")
 #ax3d.view_init(90,270)
-#ax3d.view_init(0,270)
-ax3d.view_init(0,180)
+ax3d.view_init(0,270)
+#ax3d.view_init(0,180)
 ax3d.set_xlim([-25, 25])
 ax3d.set_ylim([-25, 25])
 ax3d.set_zlim([-25, 25])
-ax3d.set_title("Phase space population at target (t $\\approx$ 7.923 years) drawn from Maxwellian at 100 au centered on vx = -26 km/s \
-    \n Target at (-.866 au, .5 au), Time Resolution Close to Target = 1500 s")
+ax3d.set_title("Phase space population at target (t $\\approx$ 9.824 years) drawn from Maxwellian at 100 au centered on vx = -26 km/s \
+    \n Target at (.9952 au, .0980 au), Time Resolution Close to Target = 1500 s")
 plt.show()
