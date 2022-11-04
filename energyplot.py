d813887f4264591f6e64a7969e173e691e5d4c43
ldyke28 <87095065+ldyke28@users.noreply.github.com>
2022-11-04 13:56:37 -0400

    Energy Plotting Code
    Wrote a short code that allows for histogram plotting of energy of points in a data file

---
+++ energyplot.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+vx = np.array([])
+vy = np.array([])
+vz = np.array([])
+f = np.array([])
+
+file = np.loadtxt("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/pi32_2p5e8_test.txt", delimiter=',')
+#file = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/5pi6_2p262502e8.txt", delimiter=',')
+
+vx = file[:,0]
+vy = file[:,1]
+vz = file[:,2]
+f = file[:,3]
+
+scaledenergy = vx[:]**2 + vy[:]**2 + vz[:]**2
+
+fig = plt.figure()
+fig.set_figwidth(10)
+fig.set_figheight(7)
+
+#plt.scatter(scaledenergy, f)
+plt.hist(scaledenergy, bins=300)
+plt.xlabel("Rescaled Particle Energy at Target Point $\\frac{2E}{m}$ ($\\frac{m^2}{s^2}$)")
+#plt.ylabel("Rescaled Phase Space Density (unitless)")
+plt.ylabel("Counts")
+plt.title("Energy Distribution for Trajectories Reaching (.9952 au, .0980 au, 0 au) at t $\\approx$ 7.923 years")
+plt.show()
