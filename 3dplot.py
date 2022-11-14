d89e61e4558eb6f0f23505bfca8c069eeacb7834
ldyke28 <87095065+ldyke28@users.noreply.github.com>
2022-11-13 21:27:18 -0500

    Plotting

--- 3dplot.py
+++ 3dplot.py
@@ -8,8 +8,8 @@ vz = np.array([])
 f = np.array([])
 
 #file = np.loadtxt("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/pi_t0.txt", delimiter=',')
-file = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/pi_t0_errortest.txt", delimiter=',')
-file2 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/GitHub/heliocode/supplementaldata1.txt", delimiter=',')
+file = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/2pi3_t0.txt", delimiter=',')
+#file2 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/GitHub/heliocode/supplementaldata1.txt", delimiter=',')
 
 #file = file[np.any(file > 1, axis=1)]
 
@@ -18,7 +18,7 @@ vy = file[:,1]
 vz = file[:,2]
 f = file[:,3]
 
-vx2 = file2[:,0]
+"""vx2 = file2[:,0]
 vy2 = file2[:,1]
 vz2 = file2[:,2]
 f2 = file2[:,3]
@@ -26,7 +26,7 @@ f2 = file2[:,3]
 vx = np.concatenate((vx, vx2))
 vy = np.concatenate((vy, vy2))
 vz = np.concatenate((vz, vz2))
-f = np.concatenate((f, f2))
+f = np.concatenate((f, f2))"""
 
 fig3d = plt.figure()
 fig3d.set_figwidth(10)
@@ -34,7 +34,7 @@ fig3d.set_figheight(7)
 ax3d = plt.axes(projection='3d')
 #scatterplot = ax3d.scatter3D(vx[:], vy[:], vz[:], c=f[:], cmap='plasma', s=.02, vmin=(.75-.243/np.e), vmax=(.75+.243*np.e))
 #scatterplot = ax3d.scatter3D(vx[:], vy[:], vz[:], c=f[:], cmap='plasma', s=.001, alpha=.15)
-scatterplot = ax3d.scatter3D(vx[:], vy[:], vz[:], c=f[:], cmap='plasma', s=.05)
+scatterplot = ax3d.scatter3D(vx[:], vy[:], vz[:], c=f[:], cmap='plasma', s=.001)
 cb = fig3d.colorbar(scatterplot)
 ax3d.set_xlabel("$v_x$ at Target Point (km/s)")
 ax3d.set_ylabel("$v_y$ at Target Point (km/s)")
@@ -46,5 +46,5 @@ ax3d.view_init(0,180)
 #ax3d.set_ylim([-25, 25])
 #ax3d.set_zlim([-25, 25])
 ax3d.set_title("Phase space population at target (t = 0 years) drawn from Maxwellian at 100 au centered on vx = -26 km/s \
-    \n Target at (-1 au, 0 au, 0 au), Time Resolution Close to Target = 1500 s")
+    \n Target at (-.5 au, .866 au, 0 au), Time Resolution Close to Target = 1500 s")
 plt.show()
