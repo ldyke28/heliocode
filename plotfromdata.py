ce3e5ad01599c319f214f30d0e57169b41aeabb6
ldyke28 <87095065+ldyke28@users.noreply.github.com>
2023-01-15 01:28:12 -0500

    Plotting

--- plotfromdata.py
+++ plotfromdata.py
@@ -10,7 +10,7 @@ filenum = 1
 
 #file = open("C:\Users\lucas\Downloads\cosexprp_pi32_1p5e8_indirect_cosexppi.txt", "r")
 #file = np.loadtxt("C:/Users/lucas/Downloads/cosexprp_pi32_1p5e8_indirect_cosexppi.txt", delimiter=',')
-file = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/cosexprp_pi32_5p5y_total_cosexppi_tclose1000.txt", delimiter=',')
+file = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/cosexprp_5pi6_p1268y_center_cosexppi_tclose1000.txt", delimiter=',')
 #file = np.loadtxt("/Users/ldyke/Downloads/drive-download-20221019T183112Z-001/cosexprp_pi32_1p5e8_indirect_cosexppi.txt", delimiter=',')
 
 
@@ -61,7 +61,8 @@ plt.title('Target at (-.866 au, .5 au), Time Resolution Close to Target = 1000 s
 plt.show()"""
 
 fsize = 18
-plt.scatter(vx[:]/1000, vy[:]/1000, c=f[:], marker='o', cmap='hsv')
+#plt.scatter(vx[:]/1000, vy[:]/1000, c=f[:], marker='o', cmap='hsv')
+plt.scatter(vx[:]/1000, vy[:]/1000, c=f[:], marker='o', cmap='hsv', norm=matplotlib.colors.LogNorm(vmin=10**(-6), vmax=10**(-2)))
 plt.rcParams.update({'font.size': fsize})
 cb = plt.colorbar()
 #cb.set_label('Time at which orbit passes through 100 au (s)')
--- pstraj.py
+++ pstraj.py
@@ -26,14 +26,14 @@ finalt = 4000000 # time to start backtracing
 #6.36674976e9 force free for cosexprp
 initialt = -2000000000
 tstep = 10000 # general time resolution
-tstepclose = 50 # time resolution for close regime
+tstepclose = 1000 # time resolution for close regime
 tstepfar = 200000 # time resolution for far regime
 phase = 0 # phase for implementing rotation of target point around sun
 
 # Location of the sun in [x,y,z] - usually this will be at 0, but this makes it flexible just in case
 # Second line is location of the point of interest in the same format (which is, generally, where we want IBEX to be)
 sunpos = np.array([0,0,0])
-theta = 45
+theta = 150
 ibexx = np.cos(theta*np.pi/180)
 ibexy = np.sin(theta*np.pi/180)
 ibexpos = np.array([ibexx*au, ibexy*au, 0])
@@ -81,12 +81,12 @@ zstart = ibexpos[2]
 # In order of how I use them - direct, indirect, center, extra one for zoomed testing
 #vxstart = np.arange(-65000, -10000, 350)
 #vystart = np.arange(-40000, 35000, 400)
-vxstart = np.arange(17000, 50000, 200)
-vystart = np.arange(2000, 45000, 300)
-#vxstart = np.arange(-25000, 25000, 500)
-#vystart = np.arange(-25000, 25000, 500)
-#vxstart = np.arange(-65000, 65000, 500)
-#vystart = np.arange(-40000, 40000, 300)
+#vxstart = np.arange(17000, 50000, 200)
+#vystart = np.arange(2000, 45000, 300)
+#vxstart = np.arange(-25000, 25000, 250)
+#vystart = np.arange(-25000, 25000, 250)
+vxstart = np.arange(-18000, -8000, 50)
+vystart = np.arange(-18000, -8000, 50)
 vzstart = 0
 if mode==3:
     startt = finalt
@@ -263,6 +263,7 @@ if mode==3:
                         fart = np.append(fart, [startt - t[kn-1]])
                         # calculating value of phase space density based on the value at the crossing of x = 100 au
                         maxwcolor = np.append(maxwcolor, [np.exp(-np.abs(attfact))*np.exp(-((backtraj[kn-1,3]+26000)**2 + backtraj[kn-1,4]**2 + backtraj[kn-1,5]**2)/(10195)**2)])
+                        #maxwcolor = np.append(maxwcolor, [np.exp(-((backtraj[kn-1,3]+26000)**2 + backtraj[kn-1,4]**2 + backtraj[kn-1,5]**2)/(10195)**2)])
                         break
                     break
 
@@ -404,7 +405,7 @@ if mode==1:
 
 if mode==3:
     # writing data to a file - need to change each time or it will overwrite previous file
-    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/cosexprp_pi4_p1268y_indirect_cosexppi_tclose50.txt", 'w')
+    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/cosexprp_5pi6_p1268y_centerzoom_cosexppi_tclose1000.txt", 'w')
     #file = open("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/norp_5pi6_p1268y_direct_cosexppi_tclose400.txt", "w")
     for i in range(farvx.size):
         file.write(str(farvx[i]/1000) + ',' + str(farvy[i]/1000) + ',' + str(maxwcolor[i]) + '\n')
