9e30d551d1817be1a0b8f3e02cb7b3e459a0cd48
ldyke28 <87095065+ldyke28@users.noreply.github.com>
2023-01-25 01:25:23 -0500

    Testing

--- pstraj.py
+++ pstraj.py
@@ -26,14 +26,14 @@ finalt = 0000000 # time to start backtracing
 #6.36674976e9 force free for cosexprp
 initialt = -5000000000
 tstep = 10000 # general time resolution
-tstepclose = 400 # time resolution for close regime
+tstepclose = 1000 # time resolution for close regime
 tstepfar = 200000 # time resolution for far regime
 phase = 0 # phase for implementing rotation of target point around sun
 
 # Location of the sun in [x,y,z] - usually this will be at 0, but this makes it flexible just in case
 # Second line is location of the point of interest in the same format (which is, generally, where we want IBEX to be)
 sunpos = np.array([0,0,0])
-theta = 85
+theta = 150
 ibexx = np.cos(theta*np.pi/180)
 ibexy = np.sin(theta*np.pi/180)
 ibexpos = np.array([ibexx*au, ibexy*au, 0])
@@ -85,8 +85,8 @@ zstart = ibexpos[2]
 #vystart = np.arange(10000, 50000, 300)
 #vxstart = np.arange(-25000, 25000, 250)
 #vystart = np.arange(-25000, 25000, 250)
-vxstart = np.arange(-12500, -7500, 40)
-vystart = np.arange(10000, 15000, 40)
+vxstart = np.arange(-5000, 00, 40)
+vystart = np.arange(-12500, -7500, 40)
 vzstart = 0
 if mode==3:
     startt = finalt
@@ -428,7 +428,7 @@ if mode==1:
 
 if mode==3:
     # writing data to a file - need to change each time or it will overwrite previous file
-    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/fluccosexprp_17pi36_0y_zoom_cosexppi_tclose1000.txt", 'w')
+    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/fluccosexprp_5pi6_0y_zoom_cosexppi_tclose1000.txt", 'w')
     #file = open("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/fluccosexprp_pi4_0y_indirect_cosexppi_tclose400.txt", "w")
     for i in range(farvx.size):
         file.write(str(farvx[i]/1000) + ',' + str(farvy[i]/1000) + ',' + str(maxwcolor[i]) + '\n')
