288073b1efc0be78dc1226f9cb4c9a218115ff3e
ldyke28 <87095065+ldyke28@users.noreply.github.com>
2023-06-20 17:57:35 -0400

    Testing

--- pstraj.py
+++ pstraj.py
@@ -24,19 +24,19 @@ oneyear = 3.15545454545*10**7
 
 # 120749800 for first force free
 # 226250200 for second force free
-finalt = 4000000 # time to start backtracing
+finalt = 000000000 # time to start backtracing
 #6.36674976e9 force free for cosexprp
 initialt = -10000000000
 tstep = 10000 # general time resolution
-tstepclose = 200 # time resolution for close regime
+tstepclose = 1000 # time resolution for close regime
 tstepfar = 200000 # time resolution for far regime
 phase = 0 # phase for implementing rotation of target point around sun
-refdist = 100 # upwind reference distance for backtraced trajectories, in au
+refdist = 300 # upwind reference distance for backtraced trajectories, in au
 
 # Location of the sun in [x,y,z] - usually this will be at 0, but this makes it flexible just in case
 # Second line is location of the point of interest in the same format (which is, generally, where we want IBEX to be)
 sunpos = np.array([0,0,0])
-theta = 150
+theta = 5.625
 ibexx = np.cos(theta*np.pi/180)
 ibexy = np.sin(theta*np.pi/180)
 ibexpos = np.array([ibexx*au, ibexy*au, 0])
@@ -84,10 +84,10 @@ zstart = ibexpos[2]
 # In order of how I use them - direct, indirect, center, extra one for zoomed testing
 #vxstart = np.arange(-62000, 10000, 550)
 #vystart = np.arange(-41000, 7000, 350)
-vxstart = np.arange(-50000, 20000, 500)
-vystart = np.arange(15000, 48000, 200)
-#vxstart = np.arange(-25000, 25000, 300)
-#vystart = np.arange(-25000, 25000, 300)
+#vxstart = np.arange(-50000, 20000, 500)
+#vystart = np.arange(15000, 48000, 200)
+vxstart = np.arange(-25000, 25000, 250)
+vystart = np.arange(-25000, 25000, 250)
 #vxstart = np.arange(5000, 10000, 25)
 #vystart = np.arange(5000, 10000, 25)
 vzstart = 0
@@ -227,7 +227,7 @@ if mode==3:
         for j in tqdm(range(vystart.size)):
             init = [xstart, ystart, zstart, vxstart[i], vystart[j], vzstart]
             # calculating trajectories for each initial condition in phase space given
-            backtraj[:,:] = odeint(dr_dt, init, t, args=(rp2,))
+            backtraj[:,:] = odeint(Lya_dr_dt, init, t, args=(LyaRP,))
             if any(np.sqrt((backtraj[:,0]-sunpos[0])**2 + (backtraj[:,1]-sunpos[1])**2 + (backtraj[:,2]-sunpos[2])**2) <= .00465*au):
                 # tells the code to not consider the trajectory if it at any point intersects the width of the sun
                 continue
@@ -436,7 +436,7 @@ if mode==1:
 
 if mode==3:
     # writing data to a file - need to change each time or it will overwrite previous file
-    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/cosexpminrp_5pi6_4e6_indirect_cosexppi_tclose200.txt", 'w')
+    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/lyarp_pi32_t0_center_cosexppi_tclose1000.txt", 'w')
     #file = open("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/p1fluccosexprp_35pi36_0y_direct_cosexppi_tclose400.txt", "w")
     for i in range(farvx.size):
         file.write(str(farvx[i]/1000) + ',' + str(farvy[i]/1000) + ',' + str(maxwcolor[i]) + '\n')
