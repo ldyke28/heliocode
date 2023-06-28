3a55224b1a6fb919cc83c30d7a0febfad3bea98d
ldyke28 <87095065+ldyke28@users.noreply.github.com>
2023-06-27 23:11:38 -0400

    Testing

--- pstraj.py
+++ pstraj.py
@@ -24,9 +24,9 @@ oneyear = 3.15545454545*10**7
 
 # 120749800 for first force free
 # 226250200 for second force free
-finalt = -45000000 # time to start backtracing
+finalt = 50000000 # time to start backtracing
 #6.36674976e9 force free for cosexprp
-initialt = -50000000000
+initialt = -5*10**(10)
 tstep = 10000 # general time resolution
 tstepclose = 500 # time resolution for close regime
 tstepfar = 200000 # time resolution for far regime
@@ -272,8 +272,8 @@ if mode==3:
 
 # single trajectory plotting code
 if mode==2:
-    indxic = -10000
-    indyic = -9750
+    indxic = -20000
+    indyic = 16000
     indzic = 00
     init = [ibexpos[0], ibexpos[1], ibexpos[2], indxic, indyic, indzic]
     print("Calculating trajectory...")
@@ -401,8 +401,8 @@ if mode==2:
     cb.ax.tick_params(labelsize=fosize)
     plt.xlabel("x (au)", fontsize=fosize)
     plt.ylabel("y (au)", fontsize=fosize)
-    plt.xlim([-2.5,15])
-    plt.ylim([-1,12])
+    plt.xlim([-2,2])
+    plt.ylim([-3,1])
     
     plt.xticks(fontsize=fosize)
     plt.yticks(fontsize=fosize)
@@ -445,7 +445,7 @@ if mode==1:
 
 if mode==3:
     # writing data to a file - need to change each time or it will overwrite previous file
-    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/lyarp_5pi6_-4p5e7s_center_cosexppi_tclose1000.txt", 'w')
+    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/lyarp_5pi6_5e7s_center_cosexppi_tclose1000.txt", 'w')
     #file = open("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/p1fluccosexprp_35pi36_0y_direct_cosexppi_tclose400.txt", "w")
     for i in range(farvx.size):
         file.write(str(farvx[i]/1000) + ',' + str(farvy[i]/1000) + ',' + str(maxwcolor[i]) + '\n')
