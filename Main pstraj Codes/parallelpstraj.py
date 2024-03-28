Scaling fix
Changed how the scaling factor in the radiation pressure force is implemented (does not affect prior results in any way, since it was just moving a multiplicative constant between two overall multiplicative terms)

Main pstraj Codes/parallelpstraj.py
@@ -374,7 +374,7 @@ def lya_abs(t,x,y,z,vr):
     tdependence = .95 + .5/(np.e**2 + 1) + .5/(np.e + 1/np.e)*np.cos(omegat - np.pi)*np.exp(np.cos(omegat - np.pi))
     # an added scale factor to adjust the total irradiance of the integral without changing the shape (adjusts total magnitude by a factor)
     # scalefactor should match divisor in first term of addfactor
-    scalefactor = .555
+    scalefactor = .333
     
     # parameters of function
     A_K = 6.523*(1 + 0.619*tdependence)
@@ -394,7 +394,7 @@ def lya_abs(t,x,y,z,vr):
     F_K = A_K * np.power(1 + np.square((vr/1000) - m_K) / (2 * K * ((del_K) ** 2)), -K - 1)
 
     #(F_K-F_R+F_bkg)/((r_E/r)**2)
-    return scalefactor*(F_K-F_R+F_bkg)/(r_E/(r2**2))*(1 - absval)
+    return scalefactor*(F_K-F_R+F_bkg)/(r_E**2/(r2**2))*(1 - absval)
 
 
 # odeint documentation: https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.odeint.html
@@ -484,7 +484,7 @@ for m in range(nprocs-1):
                         # Main code in try block
                         # If an ODEintWarning is raised, point will be set aside for testing later on
                         # calculating trajectories for each initial condition in phase space given
-                        backtraj = odeint(Var_dr_dt, init, t, args=(LyaRP4,))
+                        backtraj = odeint(Var_dr_dt, init, t, args=(lya_abs,))
                         btr = np.sqrt((backtraj[:,0]-sunpos[0])**2 + (backtraj[:,1]-sunpos[1])**2 + (backtraj[:,2]-sunpos[2])**2)
                         if any(btr <= .00465*au):
                             # tells the code to not consider the trajectory if it at any point intersects the width of the sun

Main pstraj Codes/pstraj_sphere.py
@@ -24,7 +24,7 @@ oneyear = 3.15545454545*10**7
 
 # 120749800 for first force free
 # 226250200 for second force free
-finalt = -1*oneyear # time to start backtracing
+finalt = 1*oneyear # time to start backtracing
 #6.36674976e9 force free for cosexprp
 initialt = -5*10**(10) # time in the past to which the code should backtrace
 tstep = 10000 # general time resolution
@@ -451,7 +451,7 @@ def lya_abs(t,x,y,z,vr):
     tdependence = .95 + .5/(np.e**2 + 1) + .5/(np.e + 1/np.e)*np.cos(omegat - np.pi)*np.exp(np.cos(omegat - np.pi))
     # an added scale factor to adjust the total irradiance of the integral without changing the shape (adjusts total magnitude by a factor)
     # scalefactor should match divisor in first term of addfactor
-    scalefactor = .555
+    scalefactor = .333
     
     # parameters of function
     A_K = 6.523*(1 + 0.619*tdependence)
@@ -471,7 +471,7 @@ def lya_abs(t,x,y,z,vr):
     F_K = A_K * np.power(1 + np.square((vr/1000) - m_K) / (2 * K * ((del_K) ** 2)), -K - 1)
 
     #(F_K-F_R+F_bkg)/((r_E/r)**2)
-    return scalefactor*(F_K-F_R+F_bkg)/(r_E/(r2**2))*(1 - absval)
+    return scalefactor*(F_K-F_R+F_bkg)/(r_E**2/(r2**2))*(1 - absval)
 
 
 # odeint documentation: https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.odeint.html
@@ -866,7 +866,7 @@ print('Finished')
 
 if mode==3:
     # writing data to a file - need to change each time or it will overwrite previous file
-    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/kowlyaabsrp_2pi3_-1yr_direct_cxi+cepi_tclose300_r=1au_nocut-11plot_alt_test.txt", 'w')
+    file = open("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/datafiles/kowlyaabsrp_2pi3_1yr_direct_cxi+cepi_tclose300_r=1au_nocut-11plot_interp.txt", 'w')
     #file = open("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/p1fluccosexprp_35pi36_0y_direct_cosexppi_tclose400.txt", "w")
     for i in range(farvx.size):
         # writes vx, vy, and attenuated NPSD value

Plotting Code/3dplot.py
@@ -6,16 +6,16 @@ from tqdm import tqdm
 ThreeD = True
 # Loading in the file to be unpacked
 #file = np.loadtxt("/Users/ldyke/Desktop/Dartmouth/HSResearch/Code/Kepler/Python Orbit Code/datafiles/pi_t0.txt", delimiter=',')
-file = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/3ddata/2pi3_t0_lya_direct_test.txt", delimiter=',')
+file = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/3ddata/2pi3_t0_lya_Federicodist_higherres.txt", delimiter=',')
 #file2 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/GitHub/heliocode/supplementaldata1.txt", delimiter=',')
 
 #file = file[np.any(file > 1, axis=1)]
 
 # Unpacking variables based on how they're saved in the code
-vx = file[:,0]
-vy = file[:,1]
-vz = file[:,2]
-f = file[:,3]
+vxinit = file[:,0]
+vyinit = file[:,1]
+vzinit = file[:,2]
+finit = file[:,3]
 
 """vx2 = file2[:,0]
 vy2 = file2[:,1]
@@ -30,6 +30,23 @@ f = np.concatenate((f, f2))"""
 
 # Plotting data as a 3D scatter plot
 if ThreeD == True:
+    # getting rid of points with an f value of 0
+    vx = np.array([])
+    vy = np.array([])
+    vz = np.array([])
+    f = np.array([])
+
+    fmask = (finit != 0)
+    vx = vxinit*fmask
+    vy = vyinit*fmask
+    vz = vzinit*fmask
+    f = finit*fmask
+
+    #vx = vx[~(vx == 0)]
+    #vy = vy[~(vy == 0)]
+    #vz = vz[~(vz == 0)]
+    #f = f[~(f == 0)]
+
     fig3d = plt.figure()
     fig3d.set_figwidth(10)
     fig3d.set_figheight(7)
@@ -46,9 +63,9 @@ if ThreeD == True:
     #ax3d.view_init(0,270)
     #ax3d.view_init(0,180)
     # Can restrict the limits of the plot
-    #ax3d.set_xlim([-25, 25])
-    #ax3d.set_ylim([-25, 25])
-    #ax3d.set_zlim([-25, 25])
+    ax3d.set_xlim([-55, -20])
+    #ax3d.set_ylim([-30, 30])
+    #ax3d.set_zlim([-40, 40])
     #ax3d.set_title("Phase space population at target (t = 0 years) drawn from Maxwellian at 100 au centered on vx = -26 km/s \
     #    \n Target at (1 au, 0 au, 0 au), Time Resolution Close to Target = 1500 s")
     plt.show()