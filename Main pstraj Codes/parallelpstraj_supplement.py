Fixed view angle calculation

Main pstraj Codes/parallelpstraj_supplement.py
@@ -13,16 +13,16 @@ from scipy.signal import butter, lfilter, freqz
 #####################################################################################################################################
 
 """
-This code re-calculates any points that were lose to the odeint warning in running on the cluster. The processes of the code are the
+This code re-calculates any points that were lost to the odeint warning in running on the cluster. The processes of the code are the
 same as pstraj_timedepupdate, but the initial conditions are determined by the set of (vx, vy, vz) values as given in the lost points
 file from which the code imports data.
 """
 
 #####################################################################################################################################
 
-inputfilename = "C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/lostpoints_305deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres"
-
+inputfilename = "C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/lostpoints_227deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres"
 inputfile = np.loadtxt(inputfilename + ".txt", delimiter=',')
+theta = 227 # angle with respect to upwind axis of target point
 
 # Set of initial conditions in velocity space
 # vx/vy initial conditions are sampled on a grid with chosen resolution
@@ -62,7 +62,6 @@ refdist = 115
 # Location of the sun in [x,y,z] - usually this will be at 0, but this makes it flexible just in case
 # Second line is location of the point of interest in the same format (which is, generally, where we want IBEX to be)
 sunpos = np.array([0,0,0])
-theta = 275 # angle with respect to upwind axis of target point
 ibexrad = 1 # radial distance of target point from Sun
 ibexx = ibexrad*np.cos(theta*np.pi/180)
 ibexy = ibexrad*np.sin(theta*np.pi/180)

Plotting Code/ppstrajsupp_synthesize.py
@@ -36,7 +36,7 @@ look direction, to further mimic what iBEX could observe
 
 #####################################################################################################################################
 
-fname = "-17pi36_t0_lya_Federicodist_datamu_order5_22yrsearlier_3500vres"
+fname = "293deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres"
 
 file = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/" + fname + ".txt", delimiter=',')
 suppfile = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/lostpoints_" + fname + "_revised.txt", delimiter=',')
@@ -87,7 +87,7 @@ tempH = 7500 # LISM hydrogen temperature in K
 mH = 1.6736*10**(-27) # mass of hydrogen in kg
 vthn = np.sqrt(2*1.381*10**(-29)*tempH/mH) # thermal velocity of LISM H
 
-theta = 305 # angle with respect to the upwind axis of the target point
+theta = 293 # angle with respect to the upwind axis of the target point
 vsc = 30000 # velocity of spacecraft in m/s
 thetarad = theta*np.pi/180 # expressing the value of theta in radians
 # calculating the shift of the particle velocities into the spacecraft frame
@@ -347,6 +347,15 @@ if lookdir2up < -np.pi:
 if lookdir2low < -np.pi:
     lookdir2low = lookdir2low + 2*np.pi
 
+if lookdir2up > np.pi:
+    lookdir2up = lookdir2up - 2*np.pi
+if lookdir2low > np.pi:
+    lookdir2low = lookdir2low - 2*np.pi
+if lookdir1up < -np.pi:
+    lookdir1up = lookdir1up + 2*np.pi
+if lookdir1low < -np.pi:
+    lookdir1low = lookdir1low + 2*np.pi
+
 # creating arrays to plot boundary lines at the viewing angle limits
 ld1lowmarkers = lookdir1low*np.ones((thetabounds.size))
 ld1highmarkers = lookdir1up*np.ones((thetabounds.size))
@@ -382,6 +391,7 @@ bincounterld = np.zeros((phibounds.size-1, thetabounds.size-1))
 phitrackerld = np.array([])
 thetatrackerld = np.array([])
 psdfiletrackerld = np.array([])
+
 
 for k in tqdm(range(phi.size)):
     checker = False

Plotting Code/viewanglecompositeplot.py
@@ -56,7 +56,13 @@ file7 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Clust
 file8 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/287deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres_ibexview.txt", delimiter=',')
 file9 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/293deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres_ibexview.txt", delimiter=',')
 file10 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/299deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres_ibexview.txt", delimiter=',')
-#file11 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/305deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres_ibexview.txt", delimiter=',')
+file11 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/305deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres_ibexview.txt", delimiter=',')
+file12 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/239deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres_ibexview.txt", delimiter=',')
+file13 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/311deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres_ibexview.txt", delimiter=',')
+file14 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/233deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres_ibexview.txt", delimiter=',')
+file15 = np.loadtxt("C:/Users/lucas/OneDrive/Documents/Dartmouth/HSResearch/Cluster Runs/Newest3DData/227deg_ibexshift_lya_Federicodist_datamu_order5_22yrsearlier_3500vres_ibexview.txt", delimiter=',')
+
+
 
 phi = np.array([])
 theta = np.array([])
@@ -113,10 +119,30 @@ for i in range(np.shape(file10)[0]):
     theta = np.append(theta, file10[i,1])
     flux = np.append(flux, file10[i,2])
 
-"""for i in range(np.shape(file11)[0]):
+for i in range(np.shape(file11)[0]):
     phi = np.append(phi, file11[i,0])
     theta = np.append(theta, file11[i,1])
-    flux = np.append(flux, file11[i,2])"""
+    flux = np.append(flux, file11[i,2])
+
+for i in range(np.shape(file12)[0]):
+    phi = np.append(phi, file12[i,0])
+    theta = np.append(theta, file12[i,1])
+    flux = np.append(flux, file12[i,2])
+
+for i in range(np.shape(file13)[0]):
+    phi = np.append(phi, file13[i,0])
+    theta = np.append(theta, file13[i,1])
+    flux = np.append(flux, file13[i,2])
+
+for i in range(np.shape(file14)[0]):
+    phi = np.append(phi, file14[i,0])
+    theta = np.append(theta, file14[i,1])
+    flux = np.append(flux, file14[i,2])
+
+for i in range(np.shape(file15)[0]):
+    phi = np.append(phi, file15[i,0])
+    theta = np.append(theta, file15[i,1])
+    flux = np.append(flux, file15[i,2])
 
 # creating grid points in angle space
 phibounds = np.linspace(-np.pi, np.pi, int(360/ibexvaw+1))
@@ -159,7 +185,7 @@ Lon,Lat = np.meshgrid(adjphib,adjthetab)
 
 psdtracker = np.transpose(psdtracker) # transposing the PSD value array to work with the grid
 
-im = ax.pcolormesh(Lon,Lat,psdtracker, cmap='rainbow', norm=matplotlib.colors.LogNorm(vmin=10**(1),vmax=10**(6)))
+im = ax.pcolormesh(Lon,Lat,psdtracker, cmap='rainbow')#, norm=matplotlib.colors.LogNorm(vmin=10**(1),vmax=10**(6)))
 ax.plot()
 plt.grid()
 #im = ax.scatter(phi,theta,c=fstore, cmap='rainbow')